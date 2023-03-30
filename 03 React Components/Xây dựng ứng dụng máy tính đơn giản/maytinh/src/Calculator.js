1c311623f415b00e818ccfd9b01932976a5066e4
nguyenngoc1234567 <ngoc2503pp@gmail.com>
2023-03-30 14:22:14 +0700

    danhsachhocsinh

--- 03 React Components/Xây dựng ứng dụng máy tính đơn giản/maytinh/src/Calculator.js
+++ 03 React Components/Xây dựng ứng dụng máy tính đơn giản/maytinh/src/Calculator.js
@@ -1,43 +1,43 @@
-import {useState} from "react";
+// import {useState} from "react";
 
-function Calculator(){
-const [number,setNumber] = useState(0);
-const [number2,setNumber2] = useState(0);
-const [result,setResult] = useState(0)
+// function Calculator(){
+// const [number,setNumber] = useState(0);
+// const [number2,setNumber2] = useState(0);
+// const [result,setResult] = useState(0)
 
-    function handleChangeNumber(e) {
-      setNumber(e.target.value)
+//     function handleChangeNumber(e) {
+//       setNumber(e.target.value)
 
-    }
-    function handleChangeNumber2(e) {
-      setNumber2(e.target.value)
-    }
-    function handleClickAdd(){
-      setResult( number/1 + number2/1)
-    }
-    function handleClickSub(){
-      setResult( number/1 - number2/1)
-    }
-    function handleClickMulti(){
-      setResult( number/1 * number2/1)
-    }
-    function handleClickDivide(){
-      setResult( (number/1) / (number2/1))
-    }
-    return (
-        <div>
-            <input type='number' onChange={handleChangeNumber}/>
-            <input type='number' onChange={handleChangeNumber2}/>
-            <button type='button' onClick={handleClickAdd}>+</button>
-            <button type='button' onClick={handleClickSub}>-</button>
-            <button type='button' onClick={handleClickMulti}>X</button>
-            <button type='button' onClick={handleClickDivide}>/</button>
-            <p>result: {result}</p>
-        </div>
-    )
-}
+//     }
+//     function handleChangeNumber2(e) {
+//       setNumber2(e.target.value)
+//     }
+//     function handleClickAdd(){
+//       setResult( number/1 + number2/1)
+//     }
+//     function handleClickSub(){
+//       setResult( number/1 - number2/1)
+//     }
+//     function handleClickMulti(){
+//       setResult( number/1 * number2/1)
+//     }
+//     function handleClickDivide(){
+//       setResult( (number/1) / (number2/1))
+//     }
+//     return (
+//         <div>
+//             <input type='number' onChange={handleChangeNumber}/>
+//             <input type='number' onChange={handleChangeNumber2}/>
+//             <button type='button' onClick={handleClickAdd}>+</button>
+//             <button type='button' onClick={handleClickSub}>-</button>
+//             <button type='button' onClick={handleClickMulti}>X</button>
+//             <button type='button' onClick={handleClickDivide}>/</button>
+//             <p>result: {result}</p>
+//         </div>
+//     )
+// }
 //  cách 2
-/*
+
 import {useState} from "react";
 
 
@@ -87,6 +87,6 @@ import {useState} from "react";
 }
 export default Calculator
 
-*/
 
-export default Calculator
+
+// export default Calculator
