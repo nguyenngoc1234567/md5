73962c92e10af03eb9e60dcc82fe3b75c7926687
nguyenngoc1234567 <ngoc2503pp@gmail.com>
2023-03-31 15:58:01 +0700

    démolanclick

--- 04/doimaunen/src/App.js
+++ 04/doimaunen/src/App.js
@@ -1,34 +1,63 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       color: 'black'
+//     };
+//   }
+
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ color: 'pink' });
+//     }, 5000);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div
+//           style={{
+//             backgroundColor: this.state.color,
+//             paddingTop: 20,
+//             width: 400,
+//             height: 80,
+//             margin: 'auto'
+//           }}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: 'black'
+      number: 0
     };
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ color: 'pink' });
-    }, 5000);
-  }
+  increase = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  decrease = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
 
   render() {
     return (
-      <div>
-        <div
-          style={{
-            backgroundColor: this.state.color,
-            paddingTop: 20,
-            width: 400,
-            height: 80,
-            margin: 'auto'
-          }}
-        />
+      <div style={{ textAlign: "center", padding: 30 }}>
+        <button onClick={this.decrease}>Decrease</button>
+        <span style={{ padding: 10 }}>{this.state.number}</span>
+        <button onClick={this.increase}>Increase</button>
       </div>
     );
   }
 }
 
 export default App;
+
